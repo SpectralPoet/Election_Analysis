b6c37494015193af3afe39b0b84689b89d0f8de2
Spectral Poet <mitchghendrix@gmail.com>
2021-06-29 23:01:13 -0500

    Mass 'oopsie'

---
+++ Resources/PyPoll.py
@@ -0,0 +1,11 @@
+#The data we need to retrieve:
+# 1. The total number of votes cast.
+# 2. A complete list of candidates who revieved votes.
+# 3. The percentage of votes each candidate won.
+# 4. The total number of votes each candidate won.
+# 5. The winner of the election based on popular vote.
+import csv
+import random
+import numpy
+
+
