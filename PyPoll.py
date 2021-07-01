9f09b9e9edd5cf6c071b31e604f27a80cdd9d3be
Spectral Poet <mitchghendrix@gmail.com>
2021-07-01 12:46:13 -0500

    working up to 3.4.4

--- PyPoll.py
+++ PyPoll.py
@@ -1,30 +1,50 @@
-#The data we need to retrieve:
+#-------------The data we need to retrieve:-----------------------------------
 # 1. The total number of votes cast.
 # 2. A complete list of candidates who revieved votes.
 # 3. The percentage of votes each candidate won.
 # 4. The total number of votes each candidate won.
 # 5. The winner of the election based on popular vote.
-#----------------------------------------------------------
+
+
+#------------------------Import dependencies:----------------------------------
 
 import csv
 import random
 import os
-#import numpy (numpy can't be resolved)
+#import numpy (numpy can't be resolved, anaconda issue..?)
 
-#for some reason this is running in the 'resources' folder,
-#so the first argument is unnecessary
-file_to_read = os.path.join("election_results.csv")
 
-#for some reason this is running from the recources folder..?
-file_to_write = os.path.join("..","analysis","election_analysis.txt")
+#---------------------------Create variables:-------------------------------
 
-#file to write to
-open(file_to_write, "w")
+#Get filepath to read, as string
+file_to_read = os.path.join("Resources","election_results.csv")
+#Get filepath to write to, as string
+file_to_write = os.path.join("analysis","election_analysis.txt")
 
-#Open primary data file
+
+#-----------------------------Read and analyze data:---------------------------
+
+#Create file variable to read from
 with open(file_to_read) as read_election_data:
+    
+    #create reader
+    file_reader = csv.reader(read_election_data)
     
     #To do: Analysis
 
     #Close primary data file
     read_election_data.close()
+
+
+#--------------------------Write data to file:----------------------------------
+
+#Create file variable to write to 
+with open(file_to_write, "w") as written_election_data:
+    
+    #To do: output results
+    written_election_data.write("Counties in the Election")
+    written_election_data.write("\n------------------------")
+    written_election_data.write("\nArapahoe\nDenver\nJefferson")
+
+
+    written_election_data.close()
